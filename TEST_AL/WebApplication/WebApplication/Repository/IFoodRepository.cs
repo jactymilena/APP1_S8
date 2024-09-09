20f4cac50547f35ff9b87283315a212b9e6bd46a
aliseeperreault <pera3307@usherbrooke.ca>
2024-09-09 15:37:14 -0400

    test all - restauration

---
+++ TEST_AL/WebApplication/WebApplication/Repository/IFoodRepository.cs
@@ -0,0 +1,14 @@
+﻿using WebApplication.Models;
+
+namespace WebApplication.Repository
+{
+    public interface IFoodRepository
+    {
+        void InsertOrder(Order order);
+        List<Order> FetchAllOrdersForCustomer();
+        Order FetchSpecificOrderDetails(int orderId);
+        void UpdateOrder(Order order);
+        void UpdateFoodQuantityInOrder(int orderId, int quantity);
+        void DeleteOrder(int orderId);
+    }
+}
