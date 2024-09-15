b895a254daf4a51687507f819299f07d18ff564b
aliseeperreault <pera3307@usherbrooke.ca>
2024-09-15 19:18:06 -0400

    ajout d'un test unitaire

---
+++ unit-testing-using-dotnet-test/unit-testing-using-dotnet-test/unit-testing-using-dotnet-test/PrimeService.Tests/SurveyService_GetAllAnswersBySurveyId.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormService.Tests
+{
+    internal class SurveyService_GetAllAnswersBySurveyId
+    {
+    }
+}
