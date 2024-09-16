cc75e549aad553d5471c6f299c0b29766f4fe467
aliseeperreault <pera3307@usherbrooke.ca>
2024-09-15 21:32:34 -0400

    Premiers tests unitaires

---
+++ GeneralSurvey/xUnit/SurveyService.Tests/SurveyService_GetSurvey.cs
@@ -0,0 +1,31 @@
+using Xunit;
+using Moq;
+using GeneralSurvey.Models;
+using GeneralSurvey.Services;
+using GeneralSurvey.Database;
+
+namespace Prime.UnitTests.Services
+{
+    public class SurveyService_GetSurvey
+    {
+        [Fact]
+        public void GetSurvey_ReturnsSurvey_WhenIdIsValid()
+        {
+            var mockDbHelper = new Mock<DataBaseHelper>();
+            var expectedResult = new Survey { Id = 1, Title = "Sondage 1" };
+            var surveyService = new SurveyService(new GeneralSurvey.Database.DataBaseHelper());
+            var result = surveyService.GetSurvey(1);
+
+            Assert.NotNull(result);
+            Assert.Equal(expectedResult.Id, result.Id);
+            Assert.Equal(expectedResult.Title, result.Title);
+        }
+        [Fact]
+        public void GetSurvey_ReturnsSurvey_WhenIdIsInvalid()
+        {
+            var surveyService = new SurveyService(new GeneralSurvey.Database.DataBaseHelper());
+            var result = surveyService.GetSurvey(-1);
+            Assert.Null(result);
+        }
+    }
+}
